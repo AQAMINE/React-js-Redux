cd0453dfaff2c5bf0b7bbdb37eb02f022c93b850
amine aqebli <aqebli36@gmail.com>
2022-10-14 01:27:05 +0100

    Reading & dispatching from a new slice

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,22 @@
+import { useDispatch } from 'react-redux';
+
 import classes from './Auth.module.css';
+import { authAction } from '../store';
 
 const Auth = () => {
+  const dispatch = useDispatch();
+
+  const loginHandler = event => {
+    event.preventDefault();
+
+    //We can cell authAction.login() without destruction 
+    dispatch(authAction.login());
+  }
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
